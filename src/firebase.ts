3cc9b45a7617a31a5d7d12b7467a5f99c103397b
ilaydaufelek <ilaydaufelek@gmail.com>
2025-04-15 13:33:55 +0300

    fixed

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 import { getAuth , createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { toast } from "sonner";
 const firebaseConfig = {
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 const auth =getAuth();
 
